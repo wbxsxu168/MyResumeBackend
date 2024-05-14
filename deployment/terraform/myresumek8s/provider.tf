65d036ec3ac4cbd728f01704c7643a6c40e940ec
“Sun <wbxsxu168@hotmail.com>
2024-05-14 12:21:51 -0700

    maintaining..

--- deployment/terraform/myresumek8s/provider.tf
+++ deployment/terraform/myresumek8s/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = var.region
-  access_key = var.cli_usr_access_key
+  access_key = var.cli_usr_access_key #best practice is using "aws configure" , instead of storing the key here
   secret_key = var.cli_usr_secret_key
 }
